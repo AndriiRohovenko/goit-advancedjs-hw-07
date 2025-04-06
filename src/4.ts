8835c2c24d328c0c43bbf5e23a47eee2b6e677c0
Andrii.Rohovenko <andrii.rohovenko@gmail.com>
2025-04-06 17:26:05 +0300

    add task 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,47 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`Person in house, the door is open!`);
+    } else {
+      console.log(`The door is locked !`);
+    }
+  }
+  openDoor(key: Key): void {}
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
